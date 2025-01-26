a75b889eed223c2ceeb38c35ff436bd4e07a54c4
Kostiantyn Mykhailenko <k.mykhailenko@dxc.com>
2025-01-27 00:00:00 +0200

    fix timePicker recomposition

--- app/src/main/java/com/kote/taskifyapp/ui/components/TimerPicker.kt
+++ app/src/main/java/com/kote/taskifyapp/ui/components/TimerPicker.kt
@@ -6,24 +6,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
-import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
-import com.kote.taskifyapp.data.Task
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OpenTimerPicker(
-    task: Task,
+    timePickerState: TimePickerState,
     updateTimerPicker: (Boolean) -> Unit,
     onSelectTime: (Int) -> Unit,
 ) {
-
-    val timePickerState = rememberTimePickerState(
-        is24Hour = true,
-        initialHour = task.time?.div(60) ?: 0,
-        initialMinute = task.time?.rem(60) ?: 0
-    )
-
     AlertDialog(
         onDismissRequest = { updateTimerPicker(false) },
         dismissButton = {
