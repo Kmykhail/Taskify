797caf9d1d0dab8e33d472d5da6dd80199c9428b
Kostiantyn Mykhailenko <k.mykhailenko@dxc.com>
2025-01-27 00:00:00 +0200

    add task filtration

--- app/src/main/java/com/kote/taskifyapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/kote/taskifyapp/ui/home/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.kote.taskifyapp.ui.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -25,15 +30,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.kote.taskifyapp.data.Task
 
 @Composable
 fun HomeScreen(
@@ -42,12 +45,12 @@ fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
     val tasks by viewModel.tasks.collectAsState()
-    val scope = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
             HomeTopBar(
                 onSortChange = viewModel::setSortType,
+                onFiltrationChange = viewModel::setFilterType,
                 onSwitchView = {},
                 modifier = modifier
                     .fillMaxWidth()
@@ -114,31 +117,9 @@ fun HomeScreen(
                         }
                     }
 
-                    tasks.forEach { task ->
-                        if (!task.isCompleted) {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.Start,
-                                modifier = Modifier
-                                    .padding(vertical = 2.dp)
-                                    .padding(end = 16.dp)
-                                    .fillMaxWidth()
-                                    .clickable { onNavigateToTaskDetails(task.id.toString()) }
-                            ) {
-                                Checkbox(
-                                    checked = task.isCompleted,
-                                    onCheckedChange = { viewModel.markAsCompleted(task.id) }
-                                )
-                                Text(text = task.title ?: "Untitled",)
-                                Spacer(modifier = Modifier.weight(1f))
-                                if (task.time != null) {
-                                    Icon(
-                                        imageVector = Icons.Outlined.AccessTime,
-                                        contentDescription = "Clock",
-                                        modifier = Modifier.size(24.dp)
-                                    )
-                                }
-                            }
+                    LazyColumn() {
+                        items(tasks) { task ->
+                            TaskItem(task, onNavigateToTaskDetails, viewModel::markAsCompleted)
                         }
                     }
                 }
@@ -147,8 +128,38 @@ fun HomeScreen(
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview() {
-//    Container(modifier = Modifier.padding(horizontal = 16.dp, vertical = 10.dp))
+fun TaskItem(
+    task: Task,
+    onNavigateToTaskDetails: (String) -> Unit,
+    markAsCompleted: (Int) -> Unit
+) {
+    AnimatedVisibility(
+        visible = true,
+        exit = fadeOut() + shrinkVertically()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .padding(vertical = 2.dp)
+                .padding(end = 16.dp)
+                .fillMaxWidth()
+                .clickable { onNavigateToTaskDetails(task.id.toString()) }
+        ) {
+            Checkbox(
+                checked = task.isCompleted,
+                onCheckedChange = { markAsCompleted(task.id) }
+            )
+            Text(text = task.title ?: "Untitled",)
+            Spacer(modifier = Modifier.weight(1f))
+            if (task.time != null) {
+                Icon(
+                    imageVector = Icons.Outlined.AccessTime,
+                    contentDescription = "Clock",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
 }
